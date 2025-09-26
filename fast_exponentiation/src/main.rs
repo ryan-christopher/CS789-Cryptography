70bacc60c8201a3404293e2cf1a967218cff0573
Ryan Christopher <rechristopher99@gmail.com>
2025-09-25 23:22:17 -0400

    Corrected fast_exponentiation function

--- fast_exponentiation/src/main.rs
+++ fast_exponentiation/src/main.rs
@@ -5,16 +5,15 @@
 fn fast_exponentiation(mut x: i64, mut e: i64, n: i64) -> i64 {
     // set y to default value of 1
     let mut y = 1;
-    // get base value to raise x to the power of
-    let base = x as u32;
+
     // continue until value is 0
     while e > 0 {
         println!("x: {:?}, e: {:?}, y: {:?}", x, e, y);
         // if e is even, the ending binary digit is 0, divide e
-        // by 2 and raise x by power of base
+        // by 2 and raise x by power of 2
         if e % 2 == 0 {
             e = e / 2;
-            x = x.pow(base) % n;
+            x = x.pow(2) % n;
         }
         // if e is odd, subtract 1 from e, multiply y by x,
         // and get the product mod n
@@ -23,10 +22,13 @@ fn fast_exponentiation(mut x: i64, mut e: i64, n: i64) -> i64 {
             y = (x * y) % n;
         }
     }
+    println!("x: {:?}, e: {:?}, y: {:?}", x, e, y);
     // return y when done
     y
 }
 
 fn main() {
     println!("{:?}", fast_exponentiation(2, 100, 71));
+    println!("{:?}", fast_exponentiation(7, 57, 59));
+    println!("{:?}", fast_exponentiation(2, 56, 1001));
 }
